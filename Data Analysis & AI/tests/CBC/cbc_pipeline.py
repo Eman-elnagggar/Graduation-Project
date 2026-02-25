43d521545a620cb5739ec98705be131ccc7a61ad
Eman <emanelnaggar.eman.2023@gmail.com>
2026-02-25 23:04:55 +0200

    small changes

--- Data Analysis & AI/tests/CBC/cbc_pipeline.py
+++ Data Analysis & AI/tests/CBC/cbc_pipeline.py
@@ -1,10 +1,19 @@
 import joblib
 import pandas as pd
+from sklearn.preprocessing import PowerTransformer
 
-model=joblib.load("cbc_model.pkl")
-mlb=joblib.load('mlb.pkl')
-def predicit_cbc(patient_data):
-    input_data=pd.DataFrame([patient_data.dict()])
-    prediction=model.predict(input_data)
-    predictied_labels=mlb.inverse_transform(prediction)[0]
-    return predictied_labels
+class cbc_model_prediction:
+    def __init__(self):
+        self.__model=joblib.load("cbc_model.pkl")
+        self.__mlb=joblib.load('mlb.pkl')
+        self.__pt=joblib.load('power_transformer.pkl')
+    
+    def __input_to_df(self,input_data):
+        return pd.DataFrame([input_data.dict()])
+    
+    def __preprocessing(self,input_data):
+        input_data['']
+    def predicit_cbc(patient_data):
+        prediction=model.predict(input_data)
+        predictied_labels=mlb.inverse_transform(prediction)[0]
+        return predictied_labels
