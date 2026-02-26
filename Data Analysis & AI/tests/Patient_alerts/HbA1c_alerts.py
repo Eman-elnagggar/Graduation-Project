b6674b2244c2dd3d041e77032b0eeaccd2e46e37
Eman <emanelnaggar.eman.2023@gmail.com>
2026-02-26 03:57:23 +0200

    patient alerts

---
+++ Data Analysis & AI/tests/Patient_alerts/HbA1c_alerts.py
@@ -0,0 +1,9 @@
+from HbA1c import HbA1c_pipeline
+
+def HbA1c_patient_alert(HbA1c_patient_data):
+    diagnose=HbA1c_pipeline.evaluate_HbA1c(HbA1c_patient_data)
+    alert_message=""
+    if diagnose =="disease":
+        alert_message="You have a slightly elevated blood sugar level. Try to maintain a healthy eating pattern until you see a doctor."
+    return alert_message
+        
---
+++ Data Analysis & AI/tests/Patient_alerts/tsh_alerts.py
@@ -0,0 +1,9 @@
+from TSH import tsh_pipeline
+
+def tsh_patient_alert(tsh_patient_data):
+    diagnose=tsh_pipeline.evaluate_tsh(tsh_patient_data)
+    alert_message=""
+    if diagnose =="disease":
+        alert_message="The thyroid hormone may appear to be imbalanced; urgent consultation with a doctor is advised."
+
+    return alert_message
