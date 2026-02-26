6c9a657980a323f395420290322db663f999391d
Eman <emanelnaggar.eman.2023@gmail.com>
2026-02-26 02:09:30 +0200

    CBC Pipeline

--- Data Analysis & AI/tests/CBC/cbc_pipeline.py
+++ Data Analysis & AI/tests/CBC/cbc_pipeline.py
@@ -7,13 +7,44 @@ class cbc_model_prediction:
         self.__model=joblib.load("cbc_model.pkl")
         self.__mlb=joblib.load('mlb.pkl')
         self.__pt=joblib.load('power_transformer.pkl')
+        self.__labels=['Leukopenia','Leukocytosis','Anemia','Elevated Hemoglobin','Thrombocytopenia','Thrombocytosis','Microcytosis','Macrocytosis'
+                    ,'Lymphopenia','Lymphocytosis']
     
     def __input_to_df(self,input_data):
-        return pd.DataFrame([input_data.dict()])
+        return pd.DataFrame([input_data]).astype(float)
     
     def __preprocessing(self,input_data):
-        input_data['']
-    def predicit_cbc(patient_data):
-        prediction=model.predict(input_data)
-        predictied_labels=mlb.inverse_transform(prediction)[0]
-        return predictied_labels
+        if input_data.loc[0, 'wbc'] >= 1000:
+            input_data.loc[0, 'wbc'] = input_data.loc[0, 'wbc'] / 1000
+        if input_data.loc[0, 'platelets'] >= 1000:
+            input_data.loc[0, 'platelets'] = input_data.loc[0, 'platelets'] / 1000
+        
+        input_data['abs_lymp']=(input_data['lymp'] * input_data['wbc']) / 100
+        cols_to_fix=['wbc','platelets','abs_lymp']
+        input_data[cols_to_fix] = self.__pt.transform(input_data[cols_to_fix])
+
+        del input_data['wbc']
+        del input_data['mch']
+
+        return input_data
+
+    def predicit_cbc(self,input_data):
+        input_data=self.__input_to_df(input_data)
+        input_data=self.__preprocessing(input_data)
+        sample_pred = self.__model.predict(input_data)[0]
+        predicted_diagnoses = [self.__labels[i] for i in range(len(self.__labels)) if sample_pred[i] == 1]
+        return predicted_diagnoses
+
+input_data={
+    "age":48,
+    "hb":10.9,
+    "rbc":3.75,
+    "wbc":231100,
+    "platelets":333000.0,
+    "lymp":79.0,
+    "mcv":75.6,
+    "mch":24.3,
+    "mchc":33.1,
+}
+obj=cbc_model_prediction()
+print(obj.predicit_cbc(input_data))
