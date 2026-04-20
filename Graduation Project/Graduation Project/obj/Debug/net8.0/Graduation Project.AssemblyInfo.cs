babc9b61e1e106e13829ff4da5eb350372e6072e
Karim Helal <karimhelalkh110@gmail.com>
2026-04-21 00:23:02 +0200

    Merge

--- Graduation Project/Graduation Project/obj/Debug/net8.0/Graduation Project.AssemblyInfo.cs
+++ Graduation Project/Graduation Project/obj/Debug/net8.0/Graduation Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Graduation Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92919f5c8cd082b67c4427da5a8d4bc26c6d6963")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+687b3b2e6aed8b28e417938d5f0d1c42e28c7ce7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Graduation Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Graduation Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
