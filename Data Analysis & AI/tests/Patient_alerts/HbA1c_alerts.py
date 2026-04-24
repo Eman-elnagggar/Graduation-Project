4c4dd42f5084d38755475dbcf080c5aae139b6d2
Eman <emanelnaggar.eman.2023@gmail.com>
2026-04-24 02:07:34 +0200

    Delete Old Files in Tests Diagnose

--- Data Analysis & AI/tests/Patient_alerts/HbA1c_alerts.py
+++
@@ -1,9 +0,0 @@
-from HbA1c import HbA1c_pipeline
-
-def HbA1c_patient_alert(HbA1c_patient_data)->list:
-    diagnose=HbA1c_pipeline.evaluate_HbA1c(HbA1c_patient_data)
-    alert_message=[]
-    if diagnose =="disease":
-        alert_message.append("You have a slightly elevated blood sugar level. Try to maintain a healthy eating pattern until you see a doctor.")
-    return alert_message
-        
