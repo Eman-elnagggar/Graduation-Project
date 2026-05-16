334dce9d13e069651aebbbf17d7a3ccbd7b9b276
Karim Helal <karimhelalkh110@gmail.com>
2026-05-17 01:40:25 +0300

    commit vs files

--- Graduation Project/Graduation Project/obj/Debug/net8.0/Graduation Project.AssemblyInfo.cs
+++ Graduation Project/Graduation Project/obj/Debug/net8.0/Graduation Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Graduation Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be886f41f8ebdc0f5b180fdb1cecc8f268e19fda")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+274ab33c3d2e1d3ab4425e50c4affa9dd52f05c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Graduation Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Graduation Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
