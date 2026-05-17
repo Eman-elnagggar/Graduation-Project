d83458186a1188756d110ae4280d8fa6e6351203
Karim Helal <karimhelalkh110@gmail.com>
2026-05-17 18:17:47 +0300

    edit hero section & mobile menu

--- Graduation Project/Graduation Project/obj/Debug/net8.0/Graduation Project.AssemblyInfo.cs
+++ Graduation Project/Graduation Project/obj/Debug/net8.0/Graduation Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Graduation Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0697337399f35f45338399c8b1f972d6fd05d306")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3cd72e6246f9628ff8df21b34028a7c47be67aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Graduation Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Graduation Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
